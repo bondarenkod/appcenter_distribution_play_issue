6d662bc9ba57df32f6e313b9e49e0f7f89d11c9b
Dmytro Bondarenko <bondarenkod@windowslive.com>
2021-04-16 19:05:21 +0300

    added sample projects  + apks + nugets

---
+++ play_for_platform/play_for_platform/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace play_for_platform
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios={Your App Secret};android={Your App Secret}", typeof(Analytics), typeof(Crashes), typeof(Distribute));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
